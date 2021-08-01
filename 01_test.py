ef902768d36f0c5b189f2ceef4d1668517efc108
aleksejalex <aleksejalex3@gmail.com>
2021-08-01 21:22:37 +0200

    minor changes

--- 01_test.py
+++ 01_test.py
@@ -1,16 +1,26 @@
 """
+01 - test file
+
+Just to test if the project and the GitHub are set up correctly.
 
 
 """
 
 from studyLib import *
 
+# print a concatenation of strings, access library through shortcut
 print("pi is >> " + str(np.pi))
 
+# for loop, TicToc usage
 tic()
 for i in range(600):
     print("i = " + str(i))
 
 toc()
+
+# booleans
+shakespeare = (True or (not True))  # as 'To be or not to be'
+print("The Hamlet's question has simple answer: " + str(shakespeare))
+
 
 
